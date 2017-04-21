2bcb0b1996639869a3070d1df6620285a3ef598d
timo terpstra <timoski@outlook.com>
2017-04-21 20:30:45 +0200

    made a registrasion form
    ik heb nu alles klaar liggen voor joost om alles te knoppen
    ====SUCCESS====

--- root/public/login.php
+++ root/public/login.php
@@ -33,6 +33,35 @@ include_once ("tamplates/header.php");
                 <input type="submit" value="Submit" class="button">
             </div>
         </form>
+        <form action="" method="post">
+            <div class="group-form">
+                <label for="name">Name</label>
+                <input type="text" id="name" name="name" class="textarea">
+            </div>
+            <div class="group-form">
+                <label for="surname">Surname</label>
+                <input type="text" id="surname" name="surname" class="textarea">
+            </div>
+            <div class="group-form">
+                <label for="dnummer">D-nummer</label>
+                <input type="text" id="dnummer" name="dnummer" class="textarea">
+            </div>
+            <div class="group-form">
+                <label for="email">E-mail</label>
+                <input type="text" id="email" name="email" class="textarea">
+            </div>
+            <div class="group-form">
+                <label for="password">Password</label>
+                <input type="text" id="password" name="password" class="textarea">
+            </div>
+            <div class="group-form">
+                <label for="repassword">Retype password</label>
+                <input type="text" id="repassword" name="repassword" class="textarea">
+            </div>
+            <div class="group-form">
+                <input type="submit" value="Submit" class="button">
+            </div>
+        </form>
     </div>
 <?php
 include_once ("tamplates/footer.php");
