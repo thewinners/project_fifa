bc70e78194a73b19d7a6e88b63aa093dfd1bf410
timo terpstra <timoski@outlook.com>
2017-05-03 13:38:56 +0200

    yeey gitkraken
    ga maar weer squerten

---
+++ root/public/ladder.php
@@ -0,0 +1,44 @@
+<?php
+include_once ("tamplates/header.php");
+?>
+<div class="page-title">
+    <h2>ladder</h2>
+</div>
+<div class="wrapper wrapper_page">
+    <div class="ladder column-center">
+        <div class="final">
+            <div class="l1 block"></div>
+            <div class="l2 block"></div>
+            <div class="l3 block"></div>
+            <div class="l4 block"></div>
+        </div>
+        <div class="final">
+            <div class="l5 block2"></div>
+            <div class="l6 block2"></div>
+        </div>
+        <div class="final">
+            <div class="mid7 block"></div>
+            <div class="mid8 block  column-center">
+                <h1>Finals</h1>
+            </div>
+            <div class="mid9 block"></div>
+            <div class="mid10 block"></div>
+        </div>
+        <div class="final">
+            <div class="r5 block2"></div>
+            <div class="r6 block2"></div>
+        </div>
+        <div class="final">
+            <div class="r1 block"></div>
+            <div class="r2 block"></div>
+            <div class="r3 block"></div>
+            <div class="r4 block"></div>
+        </div>
+    </div>
+    <div class="pools">
+
+    </div>
+</div>
+<?php
+include_once ("tamplates/footer.php");
+?>
