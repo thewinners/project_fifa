7140e276dff58fe385b3630416c9b378a00138e2
timo terpstra <timoski@outlook.com>
2017-05-10 01:48:55 +0200

    clock done
    clock is helemaal klaar en werkt moet aalleen nog naar de db kijken maar ide staat niet in dit bestaand

--- root/public/javascript/clock.js
+++ root/public/javascript/clock.js
@@ -1,19 +1,15 @@
+var time;
+var myTimer;
+var timer;
+var game_id = $(".page-title h2").attr("match-id");
+
 $(document).ready(function () {
-    var time;
-    var myTimer;
-    var timer;
-    var game_id = $(".page-title h2").attr("match-id");
 
     document.getElementById("start").addEventListener("click",function startTimer() {
         showTimer();
         timer = getTime();
         myTimer = setInterval(theTimer, 1000);
     });
-
-    function showTimer() {
-        document.getElementById("clock").classList.remove("hidden");
-        document.getElementById("start").classList.add("hidden");
-    }
 
     document.getElementById("pauze").addEventListener("click", function pauseTimer() {
         saveTime();
@@ -28,52 +24,57 @@ $(document).ready(function () {
         document.getElementById("pauze").classList.remove("hidden");
         document.getElementById("play").classList.add("hidden");
     });
-
-    function saveTime() {
-        $.ajax('../app/ajax/ajaxManager.php', {
-            method: "POST",
-            data: {
-                "request": 2,
-                "id" : game_id,
-                "time": timer
-            }
-        });
-    }
-
-    function getTime() {
-        $.ajax('../app/ajax/ajaxManager.php', {
-            method: "POST",
-            data: {
-                "request": 1,
-                "id" : game_id
-            }
-        }).done(function (data) {
-            timer = data;
-        });
-    }
-
-    function theTimer() {
-        timer++;
-        calulateTime = timer;
-        var hour = Math.floor(calulateTime / 3600);
-        calulateTime = calulateTime % 3600;
-        var min = Math.floor(calulateTime / 60);
-        calulateTime = calulateTime % 60;
-        if (hour != 0) {
-            time = hour + ":" + min + ":" + calulateTime;
-        }
-        else if (min != 0) {
-            time = min + ":" + calulateTime;
-        }
-        else {
-            time = calulateTime;
-        }
-
-        document.getElementById("time").innerHTML = time;
-
-        var temp = calulateTime%5;
-        if (temp == 0) {
-            saveTime();
-        }
-    }
 });
+
+function saveTime() {
+    $.ajax('../app/ajax/ajaxManager.php', {
+        method: "POST",
+        data: {
+            "request": 2,
+            "id" : game_id,
+            "time": timer
+        }
+    });
+}
+
+function getTime() {
+    $.ajax('../app/ajax/ajaxManager.php', {
+        method: "POST",
+        data: {
+            "request": 1,
+            "id" : game_id
+        }
+    }).done(function (data) {
+        timer = data;
+    });
+}
+
+function showTimer() {
+    document.getElementById("clock").classList.remove("hidden");
+    document.getElementById("start").classList.add("hidden");
+}
+
+function theTimer() {
+    timer++;
+    calulateTime = timer;
+    var hour = Math.floor(calulateTime / 3600);
+    calulateTime = calulateTime % 3600;
+    var min = Math.floor(calulateTime / 60);
+    calulateTime = calulateTime % 60;
+    if (hour != 0) {
+        time = hour + ":" + min + ":" + calulateTime;
+    }
+    else if (min != 0) {
+        time = min + ":" + calulateTime;
+    }
+    else {
+        time = calulateTime;
+    }
+
+    document.getElementById("time").innerHTML = time;
+
+    var temp = calulateTime%5;
+    if (temp == 0) {
+        saveTime();
+    }
+}
