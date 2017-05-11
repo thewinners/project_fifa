de105ca846865a1040423c95c55e1b4b00f3bfe5
JLont <d228214@edu.rocwb.nl>
2017-05-11 08:54:54 +0200

    Update player
    Update player

--- root/app/players/player.php
+++ root/app/players/player.php
@@ -16,39 +16,24 @@ function  players()
     $sql2 = "SELECT * FROM tbl_teams";
     $players = $dbc->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     $teams = $dbc->query($sql2)->fetchAll(PDO::FETCH_ASSOC);
-<<<<<<< HEAD
+
     echo "<div class='column-spaced subtitle'><p class='playerName'>Name of player</p><p class='teamName'>Team</p><p class='goals'>Goals</p></div>";
     foreach ($players as $item)
     {
         $test = \App\fetchTeam($item['team_id']);
-        if (isset($test[0]['name']) != null)
-        {
-            echo "<div class='column-spaced'><p class='playerName'>".$item['first_name'] ." ".$item['last_name']."</p><p class='teamName'>".$test[0]['name']
-                ."</p><p class='goals'>".$item ['goals']."</p></div>";
-            echo "<a class='removeButton' href='../app/players/RemovePlayer.php?playerId=".$item['id']."'>Remove " .$item["first_name"]. " ". $item["last_name"]. "</a>";
-=======
-    echo "<div class='column-spred subtitle'><p>Name of player</p><p>Team</p><p>Goals</p></div>";
-
-    foreach ($players as $item) {
-        $test = \App\fetchTeam($item['team_id']);
         if (isset($test[0]['name']) != null) {
-            echo "<div class='column-spred'><p>" . $item['first_name'] . " " . $item['last_name'] . "</p><p>" . $test[0]['name']
-                . "</p><p>" . $item ['goals'] . "</p></div>";
-    if (isset($_SESSION["logged"])) {
-        \App\UpdateUser();
-        if ($_SESSION["rights"] == "2") {
-            if ($_SESSION["team_rights"] == null) {
-                        echo "<a href='../app/players/RemovePlayer.php?playerId=" . $item['id'] . "'>Remove " . $item["first_name"] . " " . $item["last_name"] . "</a>";
+            echo "<div class='column-spaced'><p class='playerName'>" . $item['first_name'] . " " . $item['last_name'] . "</p><p class='teamName'>" . $test[0]['name']
+                . "</p><p class='goals'>" . $item ['goals'] . "</p></div>";
+
+            if (isset($_SESSION["logged"])) {
+                \App\UpdateUser();
+                if ($_SESSION["rights"] == "2") {
+                    if ($_SESSION["team_rights"] == null) {
+                        echo "<a class='removeButton' href='../app/players/RemovePlayer.php?playerId=" . $item['id'] . "'>Remove " . $item["first_name"] . " " . $item["last_name"] . "</a>";
                     }
                 }
             }
->>>>>>> master
         }
     }
 }
 ?>
-
-
-
-
-
