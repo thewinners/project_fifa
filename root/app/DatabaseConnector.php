49f1ea718453bedf0d2bcd9aaa40255ce9abf48b
dominic baeten <d228528@edu.rocwb.nl>
2017-05-11 11:29:10 +0200

    player utf8 fix

--- root/app/DatabaseConnector.php
+++ root/app/DatabaseConnector.php
@@ -3,12 +3,11 @@ namespace App;
 use PDO;
 
 require_once(__DIR__."/../config/DatabaseConfig.php");
-
 function Connect()
 {
-    $dbc = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, "root", DB_PASSWORD);
+    $dbc = new PDO("mysql:charset=utf8mb4;host=".DB_HOST.";dbname=".DB_NAME,"root", DB_PASSWORD);
     $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
     return $dbc;
+
 }
 ?>
--- root/config/DatabaseConfig.php
+++ root/config/DatabaseConfig.php
@@ -9,4 +9,5 @@ DEFINE ("DB_user", "root");
 DEFINE ("DB_PASSWORD", "");
 DEFINE ("DB_HOST", "localhost");
 DEFINE ("DB_NAME", "project_fifa");
+DEFINE ("DB_CHARSET", "utf8");
 ?>
